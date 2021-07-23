b7a87153349efc0bee99e83d69ed9bc20f0880bf
Eureka_jay <js94976988@gmail.com>
2021-07-23 22:11:57 +0900

    1.1.0 (backtotop 버튼 구현 완료)

--- js/header.js
+++
@@ -1,7 +0,0 @@
-const  toggleBtn = document.querySelector('.toggle-btn');
-const  nav_list = document.querySelector('.navbar-right');
-
-function show_list(){
-    nav_list.classList.toggle('active');
-}
-toggleBtn.addEventListener('click',show_list);
